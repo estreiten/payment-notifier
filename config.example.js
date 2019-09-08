9fc21be41570a29128aeb0e1f38194ae1227491d
asadito <streitenberger.ezequiel@gmail.com>
2019-09-07 21:24:25 -0300

    initial commit

---
+++ config.example.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mailUser: 'notifier@mail.com',
+  mailPass: 'notifierPass',
+  mailService: 'gmail',
+  mailFrom: 'notifier@mail.com',
+  mailTo: 'john.doe@mail.com',
+  btcAddress: '123jh1uih3gads8ew1233asdi'
+}
---
+++ server.js
@@ -0,0 +1,72 @@
+const nodemailer = require('nodemailer');
+const https = require('https');
+const fs = require('fs');
+const config = require('./config');
+const MS_PER_MINUTE = 60000;
+
+const transporter = nodemailer.createTransport({
+  service: config.mailService ? config.mailService : 'gmail',
+  auth: {
+    user: config.mailUser,
+    pass: config.mailPass
+  },
+  tls: {
+    rejectUnauthorized: false
+  }
+});
+const mailOptions = {
+  from: config.mailFrom,
+  to: config.mailTo,
+  subject: 'BTC transaction made!',
+  text: `Go check your account: https://live.blockcypher.com/btc/address/${config.btcAddress}/`
+};
+const httpOptions = {
+  hostname: 'api.blockcypher.com',
+  path: `/v1/btc/main/addrs/${config.btcAddress}`,
+  method: 'GET'
+};
+let notified = false;
+
+setInterval(verify, 0.2 * MS_PER_MINUTE);
+
+function verify() {
+  https.request(httpOptions, (response) => {
+    let responseData = '';
+    response.setEncoding('utf8');
+
+    response.on('data', (chunk) => {
+      responseData += chunk;
+    });
+
+    response.once('error', (err) => {
+      console.error(err);
+    });
+
+    response.on('end', () => {
+      try {
+        const transactions = JSON.parse(responseData).txrefs;
+        if (transactions.some(transaction => isToday(transaction.confirmed))) {
+          if (!notified) {
+            notified = true;
+            //send mail
+            transporter.sendMail(mailOptions, (err, info) => {
+              if (err) {
+                console.error(err);
+              } else {
+                console.info('Email sent: ' + info.response);
+              }
+            });
+          }
+        } else {  //reset the "notified" flag for the next day
+          notified = false;
+        }
+      } catch (e) {
+        console.warn('Could not parse response from options.hostname: ' + e);
+      }
+    });
+  }).end();
+}
+
+function isToday (date) {
+  return new Date(date).toDateString() === new Date().toDateString();
+}
